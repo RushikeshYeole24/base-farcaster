2b7f56c7f4b34524a15ed221cdfbc926ff18855d
Rushi-24 <2021.yeole.rushikesh@ves.ac.in>
2025-12-07 19:10:47 +0530

    update1
    update1

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -46,20 +46,20 @@ import {
     const event = data.event;
   
     switch (event.event) {
-      case "frame_added":
+      case "miniapp_added":
         if (event.notificationDetails) {
           await setUserNotificationDetails(fid, event.notificationDetails);
           await sendFrameNotification({
             fid,
-            title: "Welcome to Frames v2",
-            body: "Frame is now added to your client",
+            title: "Welcome to Bureau of Magical Things",
+            body: "Mini app is now added to your client",
           });
         } else {
           await deleteUserNotificationDetails(fid);
         }
   
         break;
-      case "frame_removed":
+      case "miniapp_removed":
         await deleteUserNotificationDetails(fid);
   
         break;
