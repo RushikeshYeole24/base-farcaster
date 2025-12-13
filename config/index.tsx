84bb7fe894079115c390d9e92a67f2c9a1b6595d
Rushi-24 <2021.yeole.rushikesh@ves.ac.in>
2025-12-13 11:51:08 +0530

    for removing project id
    for removing project id

--- config/index.tsx
+++ config/index.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { cookieStorage, createStorage, http } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { mainnet, arbitrum, base } from '@reown/appkit/networks'
